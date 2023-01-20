679118151fed36155a23f44c572ec8deff3b6026
ibrahim6755 <ibrahimghani020@gmail.com>
2023-01-20 15:38:34 +0000

    changes

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import './card.css'
+import styles from './card.module.css'
 function Card({product}) {
     return (
        <>
-        <div className="card h-95 w-100">
-            <img src={product.img} className="card-img-top" alt="..." />
+        <div className={`h-95 w-100 ${styles.card}`}>
+            <img src={product.img} className={`card-img-top ${styles.cardImg} ${styles.cardImgHover}`} alt="..." />
             <div className="card-body">
-                <h4 className="card-title">{product.title}</h4>
-                <p className="card-text mb-4">{product.description}</p>
-                <div className="flex">
+                <h4 className={styles.cardTitle}>{product.title}</h4>
+                <p className={` mb-4 ${styles.cardText}`}>{product.description}</p>
+                <div className={styles.flex}>
                 <h3 ><span className="pe-2">Rs</span>{product.price}</h3>
-                <a href="#" className="btn bucket w-100 ms-4">Add to bucket</a>
+                <a href="#" className={`btn  w-100 ms-4 ${styles.cardBucket} ${styles.cardBucketHover}`}>Add to bucket</a>
                 </div>
             </div>
         </div>
