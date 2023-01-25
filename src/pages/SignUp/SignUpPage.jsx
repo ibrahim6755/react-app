4b50a31d9c96fd1cc226ad7c1d6f1023cba22de9
ibrahim6755 <ibrahimghani020@gmail.com>
2023-01-25 09:55:06 +0000

    changes

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -4,8 +4,29 @@ import Header from '../../components/Header'
 import gif from '../../assets/img/login.gif'
 import Footer from '../../components/Footer'
 import googicon from '../../assets/img/googicon.svg'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const SignupPage = () => {
+
+  const { handleSubmit, handleReset, handleChange, handleBlur, values, errors, touched } = useFormik({
+    initialValues: {
+      name: '',
+      contact: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().min(3, 'Please provide atleast 3 characters')
+        .max(20, 'Long names are not allowed')
+        .required('Name cannot be Empty'),
+      contact: Yup.number().required('Please provide Number').min(11)
+
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+
   return (
     <>
       <div className='color' style={{ backgroundColor: 'black' }}>
@@ -20,16 +41,18 @@ const SignupPage = () => {
             <div className="col-md-4">
               <div className={styles.form}>
                 <h2 className={styles.welcome}>Welcome !</h2>
-                <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} type="text" name='name' placeholder='Name' />
-                <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} type="text" name='contact' aria-label='phone number' placeholder='+92 |' />
-                <button className={`${styles.login} my-3 p-1 py-2 px-3`}>Login</button>
+                <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} onChange={handleChange} onBlur={handleBlur} value={values.name} type="text" name='name' placeholder='Name' />
+                <p style={{ color: 'red' }}>{touched.name && errors.name ? errors.name : null}</p>
+                <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} onChange={handleChange} onBlur={handleBlur} value={values.contact} type="text" name='contact' aria-label='phone number' placeholder='+92 |' />
+                <p style={{ color: 'red' }}>{touched.contact && errors.contact ? errors.contact : null}</p>
+                <button className={`${styles.login} my-3 p-1 py-2 px-3`} onSubmit={handleSubmit}>Login</button>
                 <h5 className={`${styles.h5} ${styles.h5bf} mt-3`}>or login with</h5>
                 <a className={styles.a} href=""><img className={styles.googleicon} src={googicon} alt="" /></a>
               </div>
             </div>
           </div>
         </div>
-        <Footer/>
+        <Footer />
       </div>
 
     </>
