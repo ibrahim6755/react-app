a16cffcb96168b3f28b7fbb97ce0f1123fb25d43
ibrahim6755 <ibrahimghani020@gmail.com>
2023-01-25 06:32:48 +0000

    changes

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../SignUp/signup.module.css'
 import Header from '../../components/Header'
+import gif from '../../assets/img/login.gif'
 
 const SignupPage = () => {
   return (
@@ -9,10 +10,21 @@ const SignupPage = () => {
         <div className="container">
           <Header />
         </div>
-        <div className={styles.welcomeWrapper}>
-          <div className="row">
-            <div className="col-md-6"></div>
-            <div className="col-md-6"></div>
+        <div className={`${styles.welcomeWrapper}`}>
+          <div className={`row p-5 ${styles.row}`}>
+            <div className={`col-md-4 ${styles.gif}`}>
+              <img src={gif} alt="" />
+            </div>
+            <div className="col-md-4">
+              <div className={styles.form}>
+              <h2 className={styles.welcome}>Welcome !</h2>
+              <input className={`my-2 p-1 ${styles.input}`} type="text" />
+              <input className={`my-2 p-1 ${styles.input}`} type="text" />
+              <button></button>
+              <h5>or login with</h5>
+              <a href=""><img src="" alt="" /></a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
