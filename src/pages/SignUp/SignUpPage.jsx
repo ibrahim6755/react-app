2e0ca788e49945737e72a1e59cde3c76767ba3eb
ibrahim6755 <ibrahimghani020@gmail.com>
2023-01-26 10:31:40 +0000

    changes

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -34,11 +34,11 @@ const SignupPage = () => {
           <Header />
         </div>
         <div className={`${styles.welcomeWrapper}`}>
-          <div className={`row p-5 ${styles.row}`}>
-            <div className={`col-md-4 ${styles.gif}`}>
+          <div className={`row ${styles.row}`}>
+            <div className={`col-md-4 col-sm-3 col-lg-3 d-none d-md-block ${styles.gif}`}>
               <img src={gif} alt="" />
             </div>
-            <div className="col-md-4">
+            <div className="col-md-4 col-sm-3 col-lg-3">
               <div className={styles.form}>
                 <h2 className={styles.welcome}>Welcome !</h2>
                 <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} onChange={handleChange} onBlur={handleBlur} value={values.name} type="text" name='name' placeholder='Name' />
@@ -46,8 +46,8 @@ const SignupPage = () => {
                 <input className={` my-2 p-1 py-2 px-3 ${styles.input} ${styles.inputafter}`} onChange={handleChange} onBlur={handleBlur} value={values.contact} type="text" name='contact' aria-label='phone number' placeholder='+92 |' />
                 <p style={{ color: 'red' }}>{touched.contact && errors.contact ? errors.contact : null}</p>
                 <button className={`${styles.login} my-3 p-1 py-2 px-3`} onSubmit={handleSubmit}>Login</button>
-                <h5 className={`${styles.h5} ${styles.h5bf} mt-3`}>or login with</h5>
-                <div className={styles.div}><a className={styles.a} href=""><img className={`${styles.googleicon} ${styles.aimg}`} src={googicon} alt="" /></a></div>
+                <h5 className={`${styles.h5} mt-3 w-75 d-none d-md-block`}>or login with</h5>
+                <div className={`${styles.div} d-none d-md-block`}><a className={styles.a} href=""><img className={`${styles.googleicon} ${styles.aimg}`} src={googicon} alt="" /></a></div>
               </div>
             </div>
           </div>
